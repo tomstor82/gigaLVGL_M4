0615397039f70dacb00e759eb3792f324b42543b
Tom Storebø <tomstoreboe@gmail.com>
2025-04-24 21:27:13 +0100

    Testing: Sorted avg temp calc issue and tidied up with arrays and iterations. Set 99.9f instead of 999.0f and increased read delay to 3s to see if that helps the occasional no reading issues.

--- myLVGLdesign_M4.ino
+++ myLVGLdesign_M4.ino
@@ -10,23 +10,17 @@
 // pin 3 : Not Connected
 // pin 4 : GND
 
-// MACRO FOR DHT PINS
-#define DHT1_PIN 2   // living space r/h
-#define DHT2_PIN 3   // living space l/h
-#define DHT3_PIN 4   // shower room
-#define DHT4_PIN 5   // loft ceiling
-
 // NAMED STRUCTURE USED AS DATA-TYPE, INITIALISED WITH FAULTY DATA
 struct SensorData {
-    float temp1 = 999.9f;
-    float temp2 = 999.9f;
-    float temp3 = 999.9f;
-    float temp4 = 999.9f;
-    float rh1 = 999.9f;
-    float rh2 = 999.9f;
-    float rh3 = 999.9f;
-    float rh4 = 999.9f;
-    float avg_temp = 999.9f;
+    float temp1 = 99.9f;
+    float temp2 = 99.9f;
+    float temp3 = 99.9f;
+    float temp4 = 99.9f;
+    float rh1 = 99.9f;
+    float rh2 = 99.9f;
+    float rh3 = 99.9f;
+    float rh4 = 99.9f;
+    float avg_temp = 99.9f;
 
     MSGPACK_DEFINE_ARRAY(temp1, temp2, temp3, temp4, rh1, rh2, rh3, rh4, avg_temp);
 };
@@ -35,77 +29,47 @@ struct SensorData {
 SensorData sensorData;
 
 // GLOBAL VARIABLES
-uint16_t initial_read_delay_ms = 1000; // DHT22 sensor 2000ms recommended for accuracy but sensor will read around 360-370ms
+uint16_t read_delay_ms = 3000; // DHT22 sensor 2000ms recommended for accuracy but sensor will read around 360-370ms. 3000 set as some sensors struggle and reboot doesn't work
 uint32_t last_sensor_read_ms = 0;
 
+// OFFSETS FOR INDIVIDUAL SENSORS
 float rh_offset[4] = {0, 0, 0, 0};
 float temp_offset[4] = {0.7, 0.7, 0.7, 0.7};
 
 // SET SENSOR TYPE 22 = dht22/am2320-22
 DHTNEW setType(22);
 
-// CREATE INSTANCES OF DHT SENSORS
-DHTNEW dht1(DHT1_PIN);
-DHTNEW dht2(DHT2_PIN);
-DHTNEW dht3(DHT3_PIN);
-DHTNEW dht4(DHT4_PIN);
+// STORE DHT22 PINS TO ARRAY
+DHTNEW dht[4] = {2, 3, 4, 5}; // living space right, left, shower room and loft ceiling
 
-// READ DHT SENSOR - RETURNING 200 IF READ OK, 100 FAILED READ, 0 DELAY NOT MET
-byte readDHT(DHTNEW &sensor, float* dhtArr, byte retries) {
 
-  static uint16_t read_delay_ms = 0;
-
-  // INCREMENT READ DELAY TO SEE IF IT IS A SYNC ISSUE
-  if ( retries ) {
-     read_delay_ms += 250;
-     Serial.print("readDHT incrementing delay to: "); Serial.println(read_delay_ms+initial_read_delay_ms);
-  }
-  else {
-    read_delay_ms = 0;
-  }
-  
-  // RETURN IF READ DELAY NOT YET PASSED
-  if ( millis() - ( initial_read_delay_ms + read_delay_ms ) > last_sensor_read_ms ) {
-    last_sensor_read_ms = millis();
-  }
-  else {
-    return 0;
-  }
-  
+// READ DHT SENSOR /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+bool read_sensor(DHTNEW &sensor, float* dhtArr, byte retries) {  
 
   // IF SENSOR READY RETURN READINGS
   if (sensor.read() == DHTLIB_OK) {
     dhtArr[0] = sensor.getTemperature();
     dhtArr[1] = sensor.getHumidity();
-    return 200;
+    return 1;
   }
-  // IF RETRIES EXCEED 5 SET DATA TO INVALID AKA 999.9
-  else if (retries > 5) {
-    dhtArr[0] = 999.9f;
-    dhtArr[1] = 999.9f;
-    // RESTART SENSOR
-    sensor.powerUp();
-    RPC.println("M4 Restarting DHT22 sensor after 6 retries");
-    return 200;
+  // IF 5 RETRIES SET DATA TO INVALID AKA 99.9
+  else if (retries == 5) {
+    dhtArr[0] = 99.9f;
+    dhtArr[1] = 99.9f;
+    return 0;
   }
-  // IF SENSOR NOT READY RETURN FALSE TO INCREMENT RETRIES IN CALLBACK
+  // IF SENSOR NOT READY RETURN FALSE TO INCREMENT RETRIES
   else {
-    // RESET SENSOR AFTER 3 RETRIES
-    if (retries == 3) {
-      sensor.reset();
-      RPC.println("M4 Resetting DHT22 sensor after 3 retries");
-    }
-    // POWER DOWN SENSOR AFTER 5 RETRIES
-    else if (retries == 5) {
-      sensor.powerDown();
-      RPC.println("M4 Shutting down DHT22 sensor after 5 retries");
-    }
-    return 100;
+    return 0;
   }
 }
 
-// STORE SENSOR DATA
-void read_sensors() {
+
+// CHECK AND VALIDATE SENSOR DATA ///////////////////////////////////////////////////////////////////////////////////////////////
+void check_sensors() {
+
+  // SENSOR ARRAY
+  bool dht_read[4];
 
   // NESTED DATA ARRAY
   float dht_data[4][2];
@@ -113,63 +77,55 @@ void read_sensors() {
   // SENSOR READ RETRY ARRAY
   static byte dht_retries[4] = {0, 0, 0, 0};
 
-  byte dht1Read = readDHT(dht1, dht_data[0], dht_retries[0]);
-  byte dht2Read = readDHT(dht2, dht_data[1], dht_retries[1]);
-  byte dht3Read = readDHT(dht3, dht_data[2], dht_retries[2]);
-  byte dht4Read = readDHT(dht4, dht_data[3], dht_retries[3]);
+  // ITERATE THROUGH SENSOR ARRAY AND STORE DATA TO STRUCT OR INCREMENT RETRIES
+  for (byte i = 0; i < 3; i++) {
 
-  // LIVING ROOM R/H SENSOR
-  if (dht1Read == 200) {
-    sensorData.temp1 = dht_data[0][0];
-    sensorData.rh1 = dht_data[0][1];
-    dht_retries[0] = 0;
-  }
-  else if (dht1Read == 100) {
-    dht_retries[0]++;
-  }
-  // LIVING ROOM L/H SENSOR
-  if (dht2Read == 200) {
-    sensorData.temp2 = dht_data[1][0];
-    sensorData.rh2 = dht_data[1][1];
-    dht_retries[1] = 0;
-  }
-  else if (dht2Read == 100) {
-    dht_retries[1]++;
-  }
-  // SHOWER ROOM SENSOR
-  if (dht3Read == 200) {
-    sensorData.temp3 = dht_data[2][0];
-    sensorData.rh3 = dht_data[2][1];
-    dht_retries[2] = 0;
-  }
-  else if (dht3Read == 100) {
-    dht_retries[2]++;
-  }
-  // LOFT CEILING SENSOR
-  if (dht4Read == 200) {
-    sensorData.temp4 = dht_data[3][0];
-    sensorData.rh4 = dht_data[3][1];
-    dht_retries[3] = 0;
-  }
-  else if (dht4Read == 100) {
-    dht_retries[3]++;
+    // CALL READ FUNCTION
+    dht_read[i] = read_sensor(dht[i], dht_data[i], dht_retries[i]);
+
+    // CHECK RESULT
+    if (dht_read[i] || dht_retries[i] == 5) {
+      dht_retries[i] = 0;
+      switch (i) {
+        case 0:
+          sensorData.temp1 = dht_data[i][0];
+          sensorData.rh1 = dht_data[i][1];
+          break;
+        case 1:
+          sensorData.temp2 = dht_data[i][0];
+          sensorData.rh2 = dht_data[i][1];
+          break;
+        case 2:
+          sensorData.temp3 = dht_data[i][0];
+          sensorData.rh3 = dht_data[i][1];
+          break;
+        case 3:
+          sensorData.temp4 = dht_data[i][0];
+          sensorData.rh4 = dht_data[i][1];
+      }
+    }
+    else {
+      dht_retries[i]++;
+    }
   }
 
-  // VERIFY DATA BEFORE CALCULATION
-  if (dht1Read == 200 && dht2Read == 200 && dht4Read == 200) {
+  // VERIFY DATA BEFORE CALCULATION AVERAGE
+  if (dht_read[0] && dht_read[1] && dht_read[3]) {
     sensorData.avg_temp = (dht_data[0][0] + dht_data[1][0] + dht_data[3][0]) / 3;
   }
   else {
-    sensorData.avg_temp = 999.9f; // Set to invalid value if any sensor data is missing
+    sensorData.avg_temp = 99.9f; // Set to invalid value if any sensor data is missing
   }
 }
 
-// GET SENSOR DATA FUNCTION FOR RPC USAGE
+
+// GET SENSOR DATA FUNCTION FOR RPC USAGE ///////////////////////////////////////////////////////////////////////////////////
 SensorData getSensorData() {
   return sensorData;
 }
 
-// SETUP FUNCTION
+
+// SETUP FUNCTION ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
 
   // INITIALISE RPC ON M4 CORE
@@ -182,23 +138,23 @@ void setup() {
   RPC.bind("getSensorData", getSensorData);
 
   // SET SENSOR OFFSETS
-  dht1.setHumOffset(rh_offset[0]);
-  dht2.setHumOffset(rh_offset[1]);
-  dht3.setHumOffset(rh_offset[2]);
-  dht4.setHumOffset(rh_offset[3]);
-  dht1.setTempOffset(temp_offset[0]);
-  dht2.setTempOffset(temp_offset[1]);
-  dht3.setTempOffset(temp_offset[2]);
-  dht4.setTempOffset(temp_offset[3]);
+  for (byte i = 0; i < 3; i++) {
+    dht[i].setHumOffset(rh_offset[i]);
+    dht[i].setTempOffset(temp_offset[i]);
+  }
 
   // INITALISE TIMER
   last_sensor_read_ms = millis();
 }
 
-// LOOP FUNCTION
+
+// LOOP FUNCTION /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
 
-  read_sensors();
+  if (millis() - read_delay_ms > last_sensor_read_ms) {
+    last_sensor_read_ms = millis();
+    check_sensors();
+  }
 
-  delay(50);
+  delay(5);
 }
